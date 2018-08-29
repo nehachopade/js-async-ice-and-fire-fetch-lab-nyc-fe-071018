89260cf6ff65371c030c3a3b9d568b7a722646e3
Neha Chopade <nehachopade22@gmail.com>
2018-08-29 00:12:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
-  //write fetch request to the Game of Thrones API
+   //write fetch request to the Game of Thrones API
+   
+   fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+ 
 }
 
 function renderBooks(json) {
